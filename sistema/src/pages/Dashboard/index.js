2cd0d1c9f513c3e2f1f4d5f1c441c181222dea4a
Beatriz Marconi <beatrizmarconi.dev@gmail.com>
2022-04-27 15:23:47 -0300

    estilizando tabela clientes

--- sistema/src/pages/Dashboard/index.js
+++ sistema/src/pages/Dashboard/index.js
@@ -1,10 +1,13 @@
 import Header from "../../components/Header/index";
 import Title from "../../components/Title";
 import "./style.css"
-import {FiMessageSquare, FiPlus} from "react-icons/fi"
+import {FiMessageSquare, FiPlus, FiSearch, FiEdit2} from "react-icons/fi"
 import {Link} from "react-router-dom"
+import { useState } from "react";
 
 export default function Dashboard() {
+
+    const [chamados, setChamados ] = useState([1]);
 
 
     return(
@@ -15,15 +18,50 @@ export default function Dashboard() {
                     <FiMessageSquare size={25}/>
                 </Title>
 
-                <div className="container dashboard">
-                    <span>Nenhum chamado registrado...</span>
+                {chamados.length === 0 ? (
+                    <div className="container dashboard">
+                        <span>Nenhum chamado registrado...</span>
+                        <Link to="/new" className="new" >
+                            <FiPlus/>
+                            Novo chamado
+                        </Link>
 
-                    <Link to="/new" className="new" >
-                        <FiPlus/>
-                        Novo chamado
-                    </Link>
+                    </div>
+                ) : (
+                    <>
+                        <Link to="/new" className="new" >
+                            <FiPlus/>
+                            Novo chamado
+                        </Link>
 
-                </div>
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th scope="col">Cliente</th>
+                                    <th scope="col">Assunto</th>
+                                    <th scope="col">Status</th>
+                                    <th scope="col">Cadastrado em</th>
+                                    <th scope="col">#</th>
+                                </tr>
+                            </thead>
+
+                            <tbody>
+                                <tr>
+                                    <td data-label="Cliente">Beatriz</td>
+                                    <td data-label="Assunto">Suporte</td>
+                                    <td data-label="Status">
+                                        <span className="badge" style={{backgroundColor:'#5cb85c'}}>Em aberto</span>
+                                    </td>
+                                    <td data-label="Cadastrado">04/07/1996</td>
+                                    <td data-label="#">
+                                        <button className="action" style={{backgroundColor:'#3583f6'}}><FiSearch color="#fff" size={17}/></button>
+                                        <button className="action" style={{backgroundColor:'#f6a935'}}><FiEdit2 color="#fff" size={17}/></button>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </>
+                )}   
             </div>
         </div>
     )
